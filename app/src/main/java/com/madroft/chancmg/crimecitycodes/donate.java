5849f91aed6206f7610bb6d386a563149c27283e
Chan CMG <chancmg007>
2016-11-19 16:21:01 +0530

    mafia

---
+++ app/src/main/java/com/madroft/chancmg/crimecitycodes/donate.java
@@ -0,0 +1,17 @@
+package com.madroft.chancmg.crimecitycodes;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Chan CMG on 11/19/2016.
+ */
+public class donate extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.donate);
+    }
+}
